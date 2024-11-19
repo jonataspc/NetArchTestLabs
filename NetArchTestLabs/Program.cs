45d2d3a3f2bb2f332752a582479004d3ace990e5
JCruz <jonataspc@gmail.com>
2024-11-19 19:32:03 -0300

    First commit

---
+++ NetArchTestLabs/Program.cs
@@ -0,0 +1,28 @@
+﻿using NetArchTest.Rules;
+
+var result = Types.InCurrentDomain()
+                  .That()
+                  .HaveNameStartingWith("MyClass")
+                  .Should()
+                  .OnlyHaveDependenciesOn("System", "NetArchTestLabs")
+                  .GetResult();
+
+Console.WriteLine(result.IsSuccessful); // should be true
+Console.WriteLine(string.Join(",", result.FailingTypeNames));
+
+namespace NetArchTestLabs
+{
+    public static class MyClass
+    {
+        private static readonly MyEnum[] _values = { MyEnum.A, MyEnum.B, MyEnum.C };
+    }
+
+    public enum MyEnum
+    {
+        D,
+        A,
+        B,
+        C,
+        Z
+    }
+}
